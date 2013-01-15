b5ffc25a7f116d22b93b0cf35868220c14784e1b
Lynx <simon.schoenfeld@googlemail.com>
2013-01-15 22:28:34 +0100

    added 'HackingTest'-project

---
+++ HackingTest/HackingTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HackingTest
+{
+    class HackingTest
+    {
+        static void Main(string[] args)
+        {
+            (new Hacking.HackingMode()).start();
+        }
+    }
+}
