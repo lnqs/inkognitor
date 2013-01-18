0cf58a55510d46db5c16865115cb4898aa408ddc
Lynx <simon.schoenfeld@googlemail.com>
2013-01-18 22:10:53 +0100

    added movable Cursor

---
+++ Hacking/Cursor.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+using System.IO;
+using SdlDotNet.Graphics;
+using SdlDotNet.Graphics.Sprites;
+
+namespace Hacking
+{
+    public class Cursor : Sprite
+    {
+        private static readonly string ImageFile = "code_error.png";
+
+        public int GridX { get; set; }
+        public int GridY { get; set; }
+
+        public Cursor(Size size)
+        {
+            string filename = Path.Combine(HackingMode.ResourceDirectory, ImageFile);
+            Surface = new Surface(filename); // TODO: Resize
+
+            GridX = 0;
+            GridY = 0;
+        }
+    }
+}
--- Hacking/Hacking.cs
+++ Hacking/Hacking.cs
@@ -11,15 +11,22 @@ namespace Hacking
 
         private static readonly Size WindowSize = new Size(800, 600);
         private static readonly string WindowName = "Inkognitor";
+        private static readonly int CodeBlockColumns = 6;
+        private static readonly int CodeBlockRows = 4;
 
-        private CodeBlockGrid codeBlocks = new CodeBlockGrid(6, 4, WindowSize);
+        private CodeBlockGrid codeBlocks;
+        private Cursor cursor;
 
         public void start()
         {
+            codeBlocks = new CodeBlockGrid(CodeBlockColumns, CodeBlockRows, WindowSize);
+            cursor = new Cursor(codeBlocks.BlockPixelSize);
+
             Video.SetVideoMode(WindowSize.Width, WindowSize.Height);
             Video.WindowCaption = WindowName;
 
             Events.Tick += HandleTick;
+            Events.KeyboardDown += HandleKeyboardDown;
             Events.Quit += HandleQuit;
 
             Events.Run();
@@ -28,10 +35,31 @@ namespace Hacking
         private void HandleTick(object sender, TickEventArgs args)
         {
             codeBlocks.Update(args);
+            cursor.Position = codeBlocks[cursor.GridX, cursor.GridY].Position;
 
             Video.Screen.Blit(codeBlocks);
+            Video.Screen.Blit(cursor);
 
             Video.Screen.Update();
+        }
+
+        private void HandleKeyboardDown(object sender, KeyboardEventArgs args)
+        {
+            switch (args.Key)
+            {
+                case Key.LeftArrow:
+                    cursor.GridX = cursor.GridX > 0 ? cursor.GridX - 1 : cursor.GridX;
+                    break;
+                case Key.RightArrow:
+                    cursor.GridX = cursor.GridX < codeBlocks.Rows - 1 ? cursor.GridX + 1 : cursor.GridX;
+                    break;
+                case Key.UpArrow:
+                    cursor.GridY = cursor.GridY > 0 ? cursor.GridY - 1 : cursor.GridY;
+                    break;
+                case Key.DownArrow:
+                    cursor.GridY = cursor.GridY < codeBlocks.Columns - 1 ? cursor.GridY + 1 : cursor.GridY;
+                    break;
+            }
         }
 
         private void HandleQuit(object sender, QuitEventArgs e)
