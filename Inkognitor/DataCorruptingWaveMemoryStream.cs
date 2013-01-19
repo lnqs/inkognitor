b57fbaf6f9454ef11b5900ffc5cae99038f663c6
Lynx <simon.schoenfeld@googlemail.com>
2013-01-19 22:12:34 +0100

    corrected DataCorruptingWaveMemoryStream

--- Inkognitor/DataCorruptingWaveMemoryStream.cs
+++ Inkognitor/DataCorruptingWaveMemoryStream.cs
@@ -1,25 +1,36 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace Inkognitor
 {
     class DataCorruptingWaveMemoryStream : MemoryStream
     {
-        private static readonly int HeaderSize = 44;
+        private static readonly int HeaderSize = 45;
 
         public override void Write(byte[] buffer, int offset, int count)
         {
-            byte[] broken = new byte[buffer.Length];
-            buffer.CopyTo(broken, 0);
-
-            if (Position > HeaderSize)
+            if (Position < HeaderSize)
             {
+                int toWrite = Math.Min(count, HeaderSize - (int)Position);
+
+                base.Write(buffer, offset, toWrite);
+
+                offset += toWrite;
+                count -= toWrite;
+            }
+
+            if (count > 0)
+            {
+                byte[] broken = new byte[count];
+                Buffer.BlockCopy(buffer, offset, broken, 0, count);
+
                 for (int i = 1; i < broken.Length; i++)
                 {
                     broken[i] += broken[i - 1];
                 }
-            }
 
-            base.Write(broken, offset, count);
+                base.Write(broken, 0, broken.Length);
+            }
         }
     }
 }
