15df8d70c86715d068624b5d6ede383290974280
Lynx <simon.schoenfeld@googlemail.com>
2013-01-19 20:45:23 +0100

    added 'CodeArea'-class

---
+++ Hacking/CodeArea.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Drawing;
+using SdlDotNet.Core;
+using SdlDotNet.Graphics;
+
+namespace Hacking
+{
+    public class CodeArea
+    {
+        private CodeBlockGrid codeBlocks;
+        private Cursor cursor;
+        private Rectangle rectangle;
+
+        private Random random = new Random();
+
+        public CodeArea(int columnCount, int rowCount, Rectangle areaRectangle, Size blockSize)
+        {
+            rectangle = areaRectangle;
+            codeBlocks = new CodeBlockGrid(columnCount, rowCount, rectangle.Size);
+
+            cursor = new Cursor(blockSize);
+
+            for (int i = 0; i < codeBlocks.Height; i++)
+            {
+                InitializeCodeBlockRow(i);
+            }
+            codeBlocks.Rotated += HandleCodeBlocksRotated;
+        }
+
+        public delegate void SearchedBlockFoundHandler(object sender, EventArgs e);
+        public delegate void ErrorBlockTouchedHandler(object sender, EventArgs e);
+        public event SearchedBlockFoundHandler SearchedBlockFound;
+        public event ErrorBlockTouchedHandler ErrorBlockTouched;
+
+        public enum Direction
+        {
+            Left, Right, Up, Down
+        }
+
+        public int SearchedCodeBlock { get; set; }
+        public CodeBlockGrid CodeBlocks { get { return codeBlocks; } }
+        public Cursor Cursor { get { return cursor; } }
+        public int MaxErrorsPerCodeBlockRow { get; set; }
+        public float ErrorCodeBlockProbability { get; set; }
+
+        public void Update(TickEventArgs e)
+        {
+            codeBlocks.Update(e);
+            cursor.Position = codeBlocks[cursor.GridX, cursor.GridY].Position;
+            cursor.X += rectangle.X;
+            cursor.Y += rectangle.Y;
+        }
+
+        public void MoveCursor(Direction direction)
+        {
+            switch (direction)
+            {
+                case Direction.Left:
+                    Cursor.GridX = Math.Max(Cursor.GridX - 1, 0);
+                    break;
+                case Direction.Right:
+                    Cursor.GridX = Math.Min(Cursor.GridX + 1, codeBlocks.Width - 1);
+                    break;
+                case Direction.Up:
+                    Cursor.GridY = Math.Max(Cursor.GridY - 1, 1);
+                    break;
+                case Direction.Down:
+                    Cursor.GridY = Math.Min(Cursor.GridY + 1, codeBlocks.Height - 2);
+                    break;
+            }
+
+            CheckErrorTouched();
+        }
+
+        public void SetRandomSearchedBlock()
+        {
+            SearchedCodeBlock = random.Next(CodeBlock.Personalities);
+        }
+
+        public void CheckBlockFound()
+        {
+            if (codeBlocks[cursor.GridX, cursor.GridY].Personality == SearchedCodeBlock)
+            {
+                if (SearchedBlockFound != null)
+                {
+                    SearchedBlockFound.Invoke(this, EventArgs.Empty);
+                }
+            }
+        }
+
+        private void CheckErrorTouched()
+        {
+            if (codeBlocks[cursor.GridX, cursor.GridY].Personality == CodeBlock.PersonalityError)
+            {
+                if (ErrorBlockTouched != null)
+                {
+                    ErrorBlockTouched.Invoke(this, EventArgs.Empty);
+                }
+            }
+        }
+
+        private void InitializeCodeBlockRow(int row)
+        {
+            int errors = 0;
+
+            for (int i = 0; i < codeBlocks.Width; i++)
+            {
+                CodeBlock block = codeBlocks[i, row];
+
+                if (errors < MaxErrorsPerCodeBlockRow && random.NextDouble() < ErrorCodeBlockProbability)
+                {
+                    block.Personality = CodeBlock.PersonalityError;
+                    errors += 1;
+                }
+                else
+                {
+                    block.Personality = random.Next(CodeBlock.Personalities);
+                }
+            }
+        }
+
+        private void HandleCodeBlocksRotated(object sender, EventArgs e)
+        {
+            if (cursor.GridY > 1)
+            {
+                cursor.GridY -= 1;
+            }
+
+            InitializeCodeBlockRow(Layout.CodeBlockRowCount - 1);
+            CheckErrorTouched();
+        }
+    }
+
+    public static class CodeAreaSurfaceExtensions
+    {
+        public static Rectangle Blit(this Surface surface, CodeArea codeArea, Point destinationPoint)
+        {
+            Rectangle clippedBlocks = surface.Blit(codeArea.CodeBlocks, destinationPoint);
+            Rectangle clippedCursor = surface.Blit(
+                    codeArea.Cursor.Surface, codeArea.Cursor.Position, codeArea.Cursor.CalcClippingRectangle());
+
+            return Rectangle.Union(clippedBlocks, clippedCursor);
+        }
+    }
+}
--- Hacking/Hacking.cs
+++ Hacking/Hacking.cs
@@ -1,40 +1,36 @@
 ﻿using System;
-using System.Drawing;
 using SdlDotNet.Core;
 using SdlDotNet.Graphics;
-using SdlDotNet.Graphics.Sprites;
 using SdlDotNet.Input;
-using System.IO;
 
 namespace Hacking
 {
-    // TODO: Too much stuff in this class, split it up
     public class HackingMode
     {
         public static readonly string ResourceDirectory = "Resources";
 
         private static readonly string WindowName = "Inkognitor";
+
         private static readonly int MaxErrorsPerCodeBlockRow = 3;
         private static readonly float ErrorCodeBlockProbability = 0.1f;
 
         private InformationArea informationArea = new InformationArea(
                 Layout.LevelIndicatorPosition, Layout.CodeBlockIndicatorPosition);
-
-        private Random random = new Random();
-
-        private CodeBlockGrid codeBlocks;
-        private Cursor cursor;
+        private CodeArea codeArea = new CodeArea(
+                Layout.CodeBlockColumnCount, Layout.CodeBlockRowCount,
+                Layout.CodeArea, Layout.CodeBlockSize);
 
         private int level = 1;
 
-        private int searchedCodeBlock = 0;
-
         public void start()
         {
-            codeBlocks = new CodeBlockGrid(Layout.CodeBlockColumnCount, Layout.CodeBlockRowCount, Layout.CodeArea.Size);
-            cursor = new Cursor(Layout.CodeBlockSize);
-            InitializeCodeBlocks();
-            informationArea.DisplayedCodeBlock = CodeBlock.Surfaces[searchedCodeBlock];
+            // TODO: Check what makes sense here and what to move to constructor
+            informationArea.DisplayedCodeBlock = CodeBlock.Surfaces[codeArea.SearchedCodeBlock];
+
+            codeArea.ErrorCodeBlockProbability = ErrorCodeBlockProbability; // TODO: Make this changeable
+            codeArea.MaxErrorsPerCodeBlockRow = MaxErrorsPerCodeBlockRow; // TODO: Make this changeable
+            codeArea.SearchedBlockFound += HandleSearchedBlockFound;
+            codeArea.ErrorBlockTouched += HandleErrorBlockTouched;
 
             Video.SetVideoMode(Layout.WindowSize.Width, Layout.WindowSize.Height);
             Video.WindowCaption = WindowName;
@@ -43,49 +39,54 @@ namespace Hacking
             Events.KeyboardDown += HandleKeyboardDown;
             Events.Quit += HandleQuit;
 
-            codeBlocks.Rotated += HandleCodeBlocksRotated;
-
             Events.Run();
+        }
+
+        private void HandleSearchedBlockFound(object sender, EventArgs e)
+        {
+            level += 1;
+            codeArea.SetRandomSearchedBlock();
+            informationArea.DisplayedLevel.Text = level.ToString();
+            informationArea.DisplayedCodeBlock = CodeBlock.Surfaces[codeArea.SearchedCodeBlock];
+        }
+
+        private void HandleErrorBlockTouched(object sender, EventArgs e)
+        {
+            level -= 1;
+            codeArea.SetRandomSearchedBlock();
+            informationArea.DisplayedLevel.Text = level.ToString();
+            informationArea.DisplayedCodeBlock = CodeBlock.Surfaces[codeArea.SearchedCodeBlock];
         }
 
         private void HandleTick(object sender, TickEventArgs e)
         {
             informationArea.Update(e);
-            codeBlocks.Update(e);
-            cursor.Position = codeBlocks[cursor.GridX, cursor.GridY].Position;
-            cursor.X += Layout.CodeArea.X;
-            cursor.Y += Layout.CodeArea.Y;
+            codeArea.Update(e);
 
             Video.Screen.Blit(informationArea, Layout.InformationArea.Location);
-            Video.Screen.Blit(codeBlocks, Layout.CodeArea.Location);
-            Video.Screen.Blit(cursor.Surface, cursor.Position, cursor.CalcClippingRectangle());
+            Video.Screen.Blit(codeArea, Layout.CodeArea.Location);
 
             Video.Screen.Update();
         }
 
         private void HandleKeyboardDown(object sender, KeyboardEventArgs e)
         {
-            // TODO: refactor this method
             switch (e.Key)
             {
                 case Key.LeftArrow:
-                    cursor.GridX = cursor.GridX > 0 ? cursor.GridX - 1 : cursor.GridX;
-                    CheckErrorCodeFound();
+                    codeArea.MoveCursor(CodeArea.Direction.Left);
                     break;
                 case Key.RightArrow:
-                    cursor.GridX = cursor.GridX < codeBlocks.Width - 1 ? cursor.GridX + 1 : cursor.GridX;
-                    CheckErrorCodeFound();
+                    codeArea.MoveCursor(CodeArea.Direction.Right);
                     break;
                 case Key.UpArrow:
-                    cursor.GridY = cursor.GridY > 1 ? cursor.GridY - 1 : cursor.GridY;
-                    CheckErrorCodeFound();
+                    codeArea.MoveCursor(CodeArea.Direction.Up);
                     break;
                 case Key.DownArrow:
-                    cursor.GridY = cursor.GridY < codeBlocks.Height - 2 ? cursor.GridY + 1 : cursor.GridY;
-                    CheckErrorCodeFound();
+                    codeArea.MoveCursor(CodeArea.Direction.Down);
                     break;
                 case Key.Space:
-                    CheckCodeBlockFound();
+                    codeArea.CheckBlockFound();
                     break;
             }
         }
@@ -93,69 +94,6 @@ namespace Hacking
         private void HandleQuit(object sender, QuitEventArgs e)
         {
             Events.QuitApplication();
-        }
-
-        private void HandleCodeBlocksRotated(object sender, EventArgs e)
-        {
-            if (cursor.GridY > 1)
-            {
-                cursor.GridY -= 1;
-            }
-
-            CheckErrorCodeFound();
-
-            InitializeCodeBlockRow(Layout.CodeBlockRowCount - 1);
-        }
-
-        private void InitializeCodeBlocks()
-        {
-            for (int i = 0; i < codeBlocks.Height; i++)
-            {
-                InitializeCodeBlockRow(i);
-            }
-        }
-
-        private void InitializeCodeBlockRow(int row)
-        {
-            int errors = 0;
-
-            for (int i = 0; i < codeBlocks.Width; i++)
-            {
-                CodeBlock block = codeBlocks[i, row];
-            
-                if (errors < MaxErrorsPerCodeBlockRow && random.NextDouble() < ErrorCodeBlockProbability)
-                {
-                    block.Personality = CodeBlock.PersonalityError;
-                    errors += 1;
-                }
-                else
-                {
-                    block.Personality = random.Next(CodeBlock.Personalities);
-                }
-            }
-        }
-
-        private void CheckCodeBlockFound()
-        {
-            if (searchedCodeBlock == codeBlocks[cursor.GridX, cursor.GridY].Personality)
-            {
-                level += 1;
-                informationArea.DisplayedLevel.Text = level.ToString();
-                searchedCodeBlock = random.Next(CodeBlock.Personalities);
-                informationArea.DisplayedCodeBlock = CodeBlock.Surfaces[searchedCodeBlock];
-            }
-        }
-
-        private void CheckErrorCodeFound()
-        {
-            if (codeBlocks[cursor.GridX, cursor.GridY].Personality == CodeBlock.PersonalityError)
-            {
-                level -= 1;
-                informationArea.DisplayedLevel.Text = level.ToString();
-                searchedCodeBlock = random.Next(CodeBlock.Personalities);
-                informationArea.DisplayedCodeBlock = CodeBlock.Surfaces[searchedCodeBlock];
-                InitializeCodeBlocks();
-            }
         }
     }
 }
