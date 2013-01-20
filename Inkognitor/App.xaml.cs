f34832dde74b72e08c9c96ff4eb34b9f055755b8
Lynx <simon.schoenfeld@googlemail.com>
2013-01-20 03:24:44 +0100

    removed WebInterface for now

--- Inkognitor/App.xaml.cs
+++ Inkognitor/App.xaml.cs
@@ -6,23 +6,14 @@ namespace Inkognitor
 {
     public partial class App : Application
     {
-        private WebInterface webInterface = new WebInterface();
         private MainWindow window = new MainWindow();
         private Personality personality = new Personality();
         private HackingMode hackingMode = new HackingMode();
 
         protected override void OnStartup(StartupEventArgs e)
         {
-            webInterface.Start();
-
             window.Show();
-            window.Closed += HandleWindowClosed;
             window.TextEntered += HandleUserInput;
-        }
-
-        private void HandleWindowClosed(object sender, EventArgs e)
-        {
-            webInterface.Stop();
         }
 
         private void HandleUserInput(object sender, MainWindow.TextEnteredEventArgs e)
