40813076d8fc2714394d33896e75b7a9ac8b3f33
Lynx <simon.schoenfeld@googlemail.com>
2013-02-24 20:13:58 +0100

    A text is printed in MainWindow now

--- Inkognitor/BrokenMode.cs
+++ Inkognitor/BrokenMode.cs
@@ -1,7 +1,11 @@
-﻿namespace Inkognitor
+﻿using System;
+
+namespace Inkognitor
 {
     public class BrokenMode : IMode
     {
+        static readonly string DefaultTextFile = "GUI/DefaultText.xml";
+
         private MainWindow window;
         private Personality<DataCorruptingWaveMemoryStream> personality
                 = new Personality<DataCorruptingWaveMemoryStream>();
@@ -9,6 +13,11 @@
         public void Enter(MainWindow window_)
         {
             window = window_;
+
+            DisplayTextXMLDocument xml = new DisplayTextXMLDocument(DefaultTextFile);
+            window.titleLabel.Content = RandomReplace(xml.Title);
+            window.textBlock.Text = RandomReplace(xml.Text);
+
             window.TextEntered += HandleUserInput;
             window.Show();
         }
@@ -17,6 +26,22 @@
         {
             window.TextEntered -= HandleUserInput;
             window.Hide();
+        }
+
+        private string RandomReplace(string text)
+        {
+            char[] output = text.ToCharArray();
+            Random random = new Random();
+
+            for (int i = 0; i < output.Length; i++)
+            {
+                if (random.NextDouble() > 0.6)
+                {
+                    output[i] = (char)random.Next(0x2500, 0x25AF);
+                }
+            }
+
+            return new string(output);
         }
 
         private void HandleUserInput(object sender, MainWindow.TextEnteredEventArgs e)
--- Inkognitor/MainMode.cs
+++ Inkognitor/MainMode.cs
@@ -4,12 +4,19 @@ namespace Inkognitor
 {
     public class MainMode : IMode
     {
+        static readonly string DefaultTextFile = "GUI/DefaultText.xml";
+
         private MainWindow window;
         private Personality<MemoryStream> personality = new Personality<MemoryStream>();
 
         public void Enter(MainWindow window_)
         {
             window = window_;
+
+            DisplayTextXMLDocument xml = new DisplayTextXMLDocument(DefaultTextFile);
+            window.titleLabel.Content = xml.Title;
+            window.textBlock.Text = xml.Text;
+
             window.TextEntered += HandleUserInput;
             window.Show();
         }
