2469b465c29ec46f935c921f8d40ad293102447b
Lynx <simon.schoenfeld@googlemail.com>
2013-03-05 14:10:50 +0100

    Redesign of GUI

---
+++ Inkognitor/PercentageConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Data;
+
+namespace Inkognitor
+{
+    // Taken from http://stackoverflow.com/questions/717299/wpf-setting-the-width-and-height-as-a-percentage-value
+    public class PercentageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            double blah = System.Convert.ToDouble(parameter);
+            return System.Convert.ToDouble(value) * (System.Convert.ToDouble(parameter) / 100.0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
